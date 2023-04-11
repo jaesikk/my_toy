2d8fc84707315fae2f8e3c173a228e6c23df5e5a
엄재식 <jasic0627@gmail.com>
2023-04-12 00:19:12 +0900

    leeum_macro_v2.py complete

--- leeum_exhibit_book/leeum_macro_v2.py
+++ leeum_exhibit_book/leeum_macro_v2.py
@@ -24,11 +24,11 @@ load_dotenv()
 +) 조회는 오후타임대만 합니다.
 '''
 ####################### input value ###########################
-id = os.environ.get("ID2") #리움미술관 ID
-pw = os.environ.get("PW2") #리움미술관 PW
+id = os.environ.get("ID") #리움미술관 ID
+pw = os.environ.get("PW") #리움미술관 PW
 date_list = ['15','22'] #원하는 일자
 time_list = ['14','15','16','17']
-exhibit = 1 #원하는 전시회 (n번째)
+exhibit = 2 #원하는 전시회 (n번째)
 ###############################################################
 
 date_cnt = 0
@@ -73,96 +73,66 @@ def final_confirm():
     driver.quit()
     return 1
 
-def select_ticket(n):# 일반으로만 끊습니다. 조건 더 열기 귀찮,,
-    flag = 0
-    if n == 2:
-        ticket_doub = driver.find_element(By.XPATH, f'//*[@id="peopleType01"]/label[3]')
-        ticket_doub.click()
-        flag = 2
-
-    elif n == 1:
-        ticket_mono = driver.find_element(By.XPATH, f'//*[@id="peopleType01"]/label[2]')
-        ticket_mono.click()
-        flag = 1
-
-    else: print("selectTicket_Error !!")
-    return flag
-
 def click_refresh():
     global refresh_cnt
 
     refresh_btn = driver.find_element(By.XPATH, '//*[@id="step0"]/div/div/div[1]/ul[1]/li/button')
     refresh_btn.click()
-    time.sleep(randint(3, 4))
+    time.sleep(randint(2, 4))
     refresh_cnt += 1
-    print('재검색에 돌입합니다.')
+    # print('재검색에 돌입합니다.')
     search_date()
 
 def search_time():
     global date_cnt
-
     ########################################################
     driver.implicitly_wait(10)
+    time.sleep(0.5)
     times = driver.find_elements(By.CLASS_NAME, 'select_time')
-    for t in times:
-        time.sleep(0.1)
-        # print(t.text)
-        if '예약 가능' in t.text:
-            # print('11====>',t.text)
-            for wt in time_list:
-                if wt in t.text:
-                    print('---->',wt)
-                    t.click()
+    for idx in range(len(times)):
+
+        print('idx::',idx+1, times[idx].text)
+        for wt in time_list:
+            if wt in times[idx].text:
+                # print('wishTime:: ', wt)
+                if '예약 가능' in times[idx].text:
+                    # print('예약가능&&wishTime',idx)
+                    if idx <4:
+                        c1, c2 = 1, idx
+                    else:
+                        c1, c2 = 2, idx-4
+                    # 여기는 예약가능한 열만 온다. (동시성 오류 배제)
+                    print(wt,'시 예약 가능')
+                    val, ticket = '', ''
+                    val = driver.find_element(By.XPATH,f'//*[@id="time_body2"]/ul[{c1}]/li[{c2+1}]/a/input')
+                    lefts = val.get_attribute("value")
+                    n= int(lefts)
+                    times[idx].click()
+                    print(wt, '시 클릭 완료')
+                    # time.sleep(3)
+                    if n > 1: # 2매
+                        print('>> 2매 예약')
+                        ticket = driver.find_element(By.XPATH, f'//*[@id="peopleType01"]/label[3]')
+                    else: # 1매
+                        print('>> 1매 예약')
+                        ticket = driver.find_element(By.XPATH, f'//*[@id="peopleType01"]/label[2]')
+                    ticket.click()
+                    print('인원 선택 완료')
+
+                    # time.sleep(10)
+                    next_btn = driver.find_element(By.XPATH, '/html/body/div[1]/div[4]/div/div/div[3]/div[2]/div[2]/a')
+                    next_btn.click()
+                    final_confirm()
 
     times.clear()
-    print('times:: ',times)
-    ########################################################
-    '''
-    time.sleep(0.1)
-    print('2')
-    for i in range(1,5):
-        # test : 시간 무조건 오후거만 본다는 전제 (변경 원할 시 for문 range만 수정)
-        time.sleep(0.1) # 런타임 에러 주포인트 2
-        # time_btn = wait(driver, 5).until(
-        #     EC.presence_of_element_located((By.XPATH, f'//*[@id="time_body2"]/ul[2]/li[{i}]/a'))
-        # )
-        time_btn = driver.find_element(By.XPATH, f'//*[@id="time_body2"]/ul[2]/li[{i}]/a')
-        print('here')
-        time.sleep(0.1) # 런타임 에러 주포인트 3
-
-        # left_tickets = wait(driver, 5).until(
-        #     EC.presence_of_element_located((By.XPATH, f'//*[@id="time_body2"]/ul[2]/li[{i}]/a/input'))
-        # )
-        left_tickets = driver.find_element(By.XPATH, f'//*[@id="time_body2"]/ul[2]/li[{i}]/a/input')
-        print('this')
-        time.sleep(0.1)
-        left = left_tickets.get_attribute("value")
-        driver.implicitly_wait(10)
-        if int(left) > 1:
-            time_btn.click()
-            driver.implicitly_wait(10)
-            if select_ticket(2):
-                print('=========final step2')
-                time.sleep(0.1)
-                next_btn = driver.find_element(By.XPATH,'/html/body/div[1]/div[4]/div/div/div[3]/div[2]/div[2]/a')
-                next_btn.click()
-                final_confirm()
-        elif int(left) == 1:
-            time_btn.click()
-            if select_ticket(1):
-                print('=========final step1')
-                time.sleep(0.1)
-                next_btn = driver.find_element(By.XPATH,'/html/body/div[1]/div[4]/div/div/div[3]/div[2]/div[2]/a')
-                next_btn.click()
-                final_confirm()
-        elif int(left) == 0: # 리프레쉬 포인트
-            date_cnt += 1
-            print('잔여표::', left, '  //refresh까지:: ', date_cnt, ' :: ', (len(date_list)*4))
-        else: print('알 수 없는 오류')
-    if date_cnt == (len(date_list)*4):
+    # 예약은 가능하나, wishTime 포문 다 돌아도 없으면
+    date_cnt += 1
+    print('---------------------------semiTmp')
+    if date_cnt == len(date_list):
+        print('===================refreshPoint')
         date_cnt = 0
         click_refresh()
-    '''
+    ########################################################
 
 
 def search_date():
@@ -175,7 +145,7 @@ def search_date():
         date = day.text
         if date in date_list:
             day.click()
-            print('date clicked!! :: ', date)
+            print(date,'일 선택 완료! 시간 조회 시작')
             time.sleep(0.5)
             search_time()
     return driver
