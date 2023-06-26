95b93fd9d82f5be8a866b1e401bc119f083505a7
Noah Eom <jasic0627@gmail.com>
2023-06-26 16:24:34 +0900

    srt_book tkinter

--- srt_book/srtmacro_v2.py
+++ srt_book/srtmacro_v2.py
@@ -7,18 +7,69 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from dotenv import load_dotenv
 import os
+# load_dotenv()
+import tkinter as tk
 
-load_dotenv()
+# 입력값을 파일에 저장하는 함수
+def save_input_values(id, pw, nm, start, end, yyyymmdd, t, num_trains_to_check):
+    with open("input_values.txt", "w") as file:
+        file.write(f"ID: {id}\n")
+        file.write(f"Password: {pw}\n")
+        file.write(f"Name: {nm}\n")
+        file.write(f"Start: {start}\n")
+        file.write(f"End: {end}\n")
+        file.write(f"Date: {yyyymmdd}\n")
+        file.write(f"Time: {t}\n")
+        file.write(f"Number of trains to check: {num_trains_to_check}\n")
+    print("Input values saved successfully.")
 
-id = os.environ.get("ID1")
-pw = os.environ.get("PW1")
-nm = os.environ.get("NAME1")
-start = '통도사'
-end = '수서'
-yyyymmdd = '20230519'
-t = '14' # only 짝수
-num_trains_to_check=4
-want_reserve=False
+# 파일에서 입력값을 읽어오는 함수
+def load_input_values():
+    input_values = {}
+    with open("input_values.txt", "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            key, value = line.strip().split(": ")
+            input_values[key] = value
+    return input_values
+
+# Submit 버튼 클릭 시 실행되는 함수
+def submit_callback():
+    global n
+    # 입력값 받기
+    id = id_entry.get()
+    pw = pw_entry.get()
+    nm = nm_entry.get()
+    start = start_entry.get()
+    end = end_entry.get()
+    yyyymmdd = yyyymmdd_entry.get()
+    t = t_entry.get()
+    num_trains_to_check = num_trains_to_check_entry.get()
+
+    # 입력값 저장
+    save_input_values(id, pw, nm, start, end, yyyymmdd, t, num_trains_to_check)
+
+    # 파일에서 입력값 읽어오기
+    input_values = load_input_values()
+
+    # 결과 텍스트 업데이트
+    result_text.configure(state='normal')
+    result_text.delete('1.0', tk.END)
+    n +=1
+    result_text.insert(tk.END, f'{n}번째 log입니다.\n')
+    for key, value in input_values.items():
+        result_text.insert(tk.END, f"{key}: {value}\n")
+    result_text.configure(state='disabled')
+
+    #main 실행
+    main(id, pw, nm, start, end, yyyymmdd, t, num_trains_to_check)
+
+def main(id, pw, nm, start, end, yyyymmdd, t, num_trains_to_check):
+    browser = webdriver.Chrome()
+    # driver = open_browser()
+    driver = login(browser, id, pw)
+    search_train(driver, start, end, yyyymmdd, t) #기차 출발 시간은 반드시 짝수
+
 def open_browser():
     driver = webdriver.Chrome("chromedriver")
     return driver
@@ -26,6 +77,7 @@ def open_browser():
 
 def login(driver, login_id, login_psw):
     driver.get('https://etk.srail.co.kr/cmc/01/selectLoginForm.do')
+    print('login 진입완료: ', login_id, login_psw)
     driver.implicitly_wait(15)
     driver.find_element(By.ID, 'srchDvNm01').send_keys(str(login_id))
     driver.find_element(By.ID, 'hmpgPwdCphd01').send_keys(str(login_psw))
@@ -108,13 +160,185 @@ def search_train(driver, dpt_stn, arr_stn, dpt_dt, dpt_tm):
             break
     return driver
 
+n = 0
+# tkinter 윈도우 생성
+window = tk.Tk()
+window.title("Input Values")
+
+# 입력값을 불러오기 위해 파일에서 입력값 읽어오기
+input_values = load_input_values()
+
+# ID 입력 필드
+id_label = tk.Label(window, text="ID:")
+id_label.pack()
+id_entry = tk.Entry(window)
+id_entry.insert(tk.END, input_values.get("ID", ""))  # 이전 값이 있으면 입력 창에 표시
+id_entry.pack()
+
+# Password 입력 필드
+pw_label = tk.Label(window, text="Password:")
+pw_label.pack()
+pw_entry = tk.Entry(window)
+pw_entry.insert(tk.END, input_values.get("Password", ""))
+pw_entry.pack()
+
+# Name 입력 필드
+nm_label = tk.Label(window, text="Name:")
+nm_label.pack()
+nm_entry = tk.Entry(window)
+nm_entry.insert(tk.END, input_values.get("Name", ""))
+nm_entry.pack()
+
+# Start 입력 필드
+start_label = tk.Label(window, text="Start:")
+start_label.pack()
+start_entry = tk.Entry(window)
+start_entry.insert(tk.END, input_values.get("Start", ""))
+start_entry.pack()
+
+# End 입력 필드
+end_label = tk.Label(window, text="End:")
+end_label.pack()
+end_entry = tk.Entry(window)
+end_entry.insert(tk.END, input_values.get("End", ""))
+end_entry.pack()
+
+# Date 입력 필드
+yyyymmdd_label = tk.Label(window, text="Date (YYYYMMDD):")
+yyyymmdd_label.pack()
+yyyymmdd_entry = tk.Entry(window)
+yyyymmdd_entry.insert(tk.END, input_values.get("Date", ""))
+yyyymmdd_entry.pack()
+
+# Time 입력 필드
+t_label = tk.Label(window, text="Time (even number):")
+t_label.pack()
+t_entry = tk.Entry(window)
+t_entry.insert(tk.END, input_values.get("Time", ""))
+t_entry.pack()
+
+# Number of trains to check 입력 필드
+num_trains_to_check_label = tk.Label(window, text="Number of trains to check:")
+num_trains_to_check_label.pack()
+num_trains_to_check_entry = tk.Entry(window)
+num_trains_to_check_entry.insert(tk.END, input_values.get("Number of trains to check", ""))
+num_trains_to_check_entry.pack()
+
+# Submit 버튼
+submit_button = tk.Button(window, text="Submit", command=submit_callback)
+submit_button.pack()
+
+# 결과 텍스트 박스
+result_text = tk.Text(window, height=10, width=50)
+result_text.configure(state='disabled')
+result_text.pack()
+
+# 이벤트 루프 실행
+window.mainloop()
+
+
+
+
+# id = os.environ.get("ID1")
+# pw = os.environ.get("PW1")
+# nm = os.environ.get("NAME1")
+# start = '통도사'
+# end = '수서'
+# yyyymmdd = '20230519'
+# t = '14' # only 짝수
+# num_trains_to_check=4
+want_reserve=False
+
 ### ID, PW : SRT 계정
 ### 23행 search_train에서 세부 조정
-if __name__ == "__main__":
-    options = webdriver.ChromeOptions()
-    # options.add_experimental_option("excludeSwitches", ["enable-logging"])
-    # browser = webdriver.Chrome(options=options)
-    browser = webdriver.Chrome()
-    # driver = open_browser()
-    driver = login(browser, id, pw)
-    search_train(driver, start, end, yyyymmdd, t) #기차 출발 시간은 반드시 짝수
+# if __name__ == "__main__":
+#     options = webdriver.ChromeOptions()
+#     # options.add_experimental_option("excludeSwitches", ["enable-logging"])
+#     # browser = webdriver.Chrome(options=options)
+#     browser = webdriver.Chrome()
+#     # driver = open_browser()
+#     driver = login(browser, id, pw)
+#     search_train(driver, start, end, yyyymmdd, t) #기차 출발 시간은 반드시 짝수
+
+###############################
+# def submit():
+#     global id, pw, nm, start, end, yyyymmdd, t, num_trains_to_check
+#     id = entry_id.get()
+#     pw = entry_pw.get()
+#     nm = entry_nm.get()
+#     start = entry_start.get()
+#     end = entry_end.get()
+#     yyyymmdd = entry_yyyymmdd.get()
+#     t = entry_t.get()
+#     num_trains_to_check = int(entry_num_trains_to_check.get()) if entry_num_trains_to_check.get() else 0
+#
+#     root.destroy()
+#     # if __name__ == "__main__":
+#     # options = webdriver.ChromeOptions()
+#     # options.add_experimental_option("excludeSwitches", ["enable-logging"])
+#     # browser = webdriver.Chrome(options=options)
+#     browser = webdriver.Chrome()
+#     # driver = open_browser()
+#     driver = login(browser, id, pw)
+#     search_train(driver, start, end, yyyymmdd, t)  # 기차 출발 시간은 반드시 짝수
+#
+#
+# #
+# # root = tk.Tk()
+# # root.title("입력값 설정")
+# # root.geometry("300x400")
+# #
+# # label_id = tk.Label(root, text="ID:")
+# # label_id.pack()
+# # entry_id = tk.Entry(root)
+# # entry_id.pack()
+# #
+# # label_pw = tk.Label(root, text="PW:")
+# # label_pw.pack()
+# # entry_pw = tk.Entry(root, show="*")
+# # entry_pw.pack()
+# #
+# # label_nm = tk.Label(root, text="NAME:")
+# # label_nm.pack()
+# # entry_nm = tk.Entry(root)
+# # entry_nm.pack()
+# #
+# # label_start = tk.Label(root, text="출발역:")
+# # label_start.pack()
+# # entry_start = tk.Entry(root)
+# # entry_start.pack()
+# #
+# # label_end = tk.Label(root, text="도착역:")
+# # label_end.pack()
+# # entry_end = tk.Entry(root)
+# # entry_end.pack()
+# #
+# # label_yyyymmdd = tk.Label(root, text="날짜 (YYYYMMDD):")
+# # label_yyyymmdd.pack()
+# # entry_yyyymmdd = tk.Entry(root)
+# # entry_yyyymmdd.pack()
+# #
+# # label_t = tk.Label(root, text="시간 (짝수):")
+# # label_t.pack()
+# # entry_t = tk.Entry(root)
+# # entry_t.pack()
+# #
+# # label_num_trains_to_check = tk.Label(root, text="확인할 열차 수:")
+# # label_num_trains_to_check.pack()
+# # entry_num_trains_to_check = tk.Entry(root)
+# # entry_num_trains_to_check.pack()
+# #
+# # button_submit = tk.Button(root, text="Submit", command=submit)
+# # button_submit.pack()
+# #
+# # root.mainloop()
+# #
+# # print("입력된 값:")
+# # print("ID:", id)
+# # print("PW:", pw)
+# # print("NAME:", nm)
+# # print("출발역:", start)
+# # print("도착역:", end)
+# # print("날짜:", yyyymmdd)
+# # print("시간:", t)
+# # print("확인할 열차 수:", num_trains_to_check)
--- srt_book/test.py
+++ srt_book/test.py
@@ -1,6 +1,168 @@
-import telegram
+import os
+import tkinter as tk
 
-print('>> start')
-bot = telegram.Bot(token='5935426656:AAGg6sMVUoTmHG2AHqWnP87ZT7rAZ9nG6sI')
-bot.send_message(chat_id="6188222004", text=f"ㅆㅣ바 안되긴 왜 안돼")
-print('>> end')
+# 입력값을 파일에 저장하는 함수
+def save_input_values(id, pw, nm, start, end, yyyymmdd, t, num_trains_to_check):
+    with open("input_values.txt", "w") as file:
+        file.write(f"ID: {id}\n")
+        file.write(f"Password: {pw}\n")
+        file.write(f"Name: {nm}\n")
+        file.write(f"Start: {start}\n")
+        file.write(f"End: {end}\n")
+        file.write(f"Date: {yyyymmdd}\n")
+        file.write(f"Time: {t}\n")
+        file.write(f"Number of trains to check: {num_trains_to_check}\n")
+    print("Input values saved successfully.")
+
+# 파일에서 입력값을 읽어오는 함수
+def load_input_values():
+    input_values = {}
+    with open("input_values.txt", "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            key, value = line.strip().split(": ")
+            input_values[key] = value
+    return input_values
+
+# Submit 버튼 클릭 시 실행되는 함수
+def submit_callback():
+    global n
+    # 입력값 받기
+    id = id_entry.get()
+    pw = pw_entry.get()
+    nm = nm_entry.get()
+    start = start_entry.get()
+    end = end_entry.get()
+    yyyymmdd = yyyymmdd_entry.get()
+    t = t_entry.get()
+    num_trains_to_check = num_trains_to_check_entry.get()
+
+    # 입력값 저장
+    save_input_values(id, pw, nm, start, end, yyyymmdd, t, num_trains_to_check)
+
+    # 파일에서 입력값 읽어오기
+    input_values = load_input_values()
+
+    # 결과 텍스트 업데이트
+    result_text.configure(state='normal')
+    result_text.delete('1.0', tk.END)
+    n +=1
+    result_text.insert(tk.END, f'{n}번째 log입니다.\n')
+    for key, value in input_values.items():
+        result_text.insert(tk.END, f"{key}: {value}\n")
+    result_text.configure(state='disabled')
+
+n = 0
+# tkinter 윈도우 생성
+window = tk.Tk()
+window.title("Input Values")
+
+# 입력값을 불러오기 위해 파일에서 입력값 읽어오기
+input_values = load_input_values()
+
+# ID 입력 필드
+id_label = tk.Label(window, text="ID:")
+id_label.pack()
+id_entry = tk.Entry(window)
+id_entry.insert(tk.END, input_values.get("ID", ""))  # 이전 값이 있으면 입력 창에 표시
+id_entry.pack()
+
+# Password 입력 필드
+pw_label = tk.Label(window, text="Password:")
+pw_label.pack()
+pw_entry = tk.Entry(window)
+pw_entry.insert(tk.END, input_values.get("Password", ""))
+pw_entry.pack()
+
+# Name 입력 필드
+nm_label = tk.Label(window, text="Name:")
+nm_label.pack()
+nm_entry = tk.Entry(window)
+nm_entry.insert(tk.END, input_values.get("Name", ""))
+nm_entry.pack()
+
+# Start 입력 필드
+start_label = tk.Label(window, text="Start:")
+start_label.pack()
+start_entry = tk.Entry(window)
+start_entry.insert(tk.END, input_values.get("Start", ""))
+start_entry.pack()
+
+# End 입력 필드
+end_label = tk.Label(window, text="End:")
+end_label.pack()
+end_entry = tk.Entry(window)
+end_entry.insert(tk.END, input_values.get("End", ""))
+end_entry.pack()
+
+# Date 입력 필드
+yyyymmdd_label = tk.Label(window, text="Date (YYYYMMDD):")
+yyyymmdd_label.pack()
+yyyymmdd_entry = tk.Entry(window)
+yyyymmdd_entry.insert(tk.END, input_values.get("Date", ""))
+yyyymmdd_entry.pack()
+
+# Time 입력 필드
+t_label = tk.Label(window, text="Time (even number):")
+t_label.pack()
+t_entry = tk.Entry(window)
+t_entry.insert(tk.END, input_values.get("Time", ""))
+t_entry.pack()
+
+# Number of trains to check 입력 필드
+num_trains_to_check_label = tk.Label(window, text="Number of trains to check:")
+num_trains_to_check_label.pack()
+num_trains_to_check_entry = tk.Entry(window)
+num_trains_to_check_entry.insert(tk.END, input_values.get("Number of trains to check", ""))
+num_trains_to_check_entry.pack()
+
+# Submit 버튼
+submit_button = tk.Button(window, text="Submit", command=submit_callback)
+submit_button.pack()
+
+# 결과 텍스트 박스
+result_text = tk.Text(window, height=10, width=50)
+result_text.configure(state='disabled')
+result_text.pack()
+
+# 이벤트 루프 실행
+window.mainloop()
+
+
+######################################################################
+# import tkinter as tk
+#
+# def submit_callback():
+#     recent_value = entry.get()
+#     print("Submit 버튼이 눌렸습니다.")
+#     print("가장 최근 값:", recent_value)
+#
+#     # 최근 값을 파일에 저장
+#     with open("recent_value.txt", "w") as file:
+#         file.write(recent_value)
+#
+#     print("__main__ 블록 :: 검색이 실행됩니다.")
+#
+# def read_recent_value():
+#     # 파일에서 최근 값을 읽어옴
+#     with open("recent_value.txt", "r") as file:
+#         recent_value = file.read()
+#     print("파일에서 읽어온 최근 값:", recent_value)
+#
+# # tkinter 윈도우 생성
+# window = tk.Tk()
+#
+# # 입력 필드 생성
+# entry = tk.Entry(window)
+# entry.pack()
+#
+# # submit 버튼 생성
+# submit_button = tk.Button(window, text="Submit", command=submit_callback)
+# submit_button.pack()
+#
+# # 파일에서 최근 값을 읽어오는 예시
+# read_value_button = tk.Button(window, text="파일에서 최근 값 읽기", command=read_recent_value)
+# read_value_button.pack()
+#
+# # 이벤트 루프 실행
+# window.mainloop()
